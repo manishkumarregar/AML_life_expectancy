68f60aab58195279f28c9ce5fa4792df0281ace8
Nilesh Vaishnav <nilesh.iitg@gmail.com>
2018-04-16 22:53:41 +0530

    Modularize the clustering algorithms

--- affinityPropagation.py
+++ affinityPropagation.py
@@ -1,35 +1,42 @@
+import os
 from sklearn.datasets.samples_generator import make_blobs
 from sklearn.cluster import AffinityPropagation
 import matplotlib.pyplot as plt
 from itertools import cycle
 from readData import load_data
+from dim_reduction import reduce_dim
+from saveResults import save_fig
 # Make Dummy Data
 # centers = [[1, 1], [-1, -1], [1, -1]]
 # X, labels_true = make_blobs(n_samples=300, centers=centers, cluster_std=0.5, random_state=0)
 
-X , days_to_death = load_data()
-# Setup Affinity Propagation
-af = AffinityPropagation(preference=-50).fit(X)
-cluster_centers_indices = af.cluster_centers_indices_
-labels = af.labels_
+def cluster(X,fig,ax):
 
-print(cluster_centers_indices)
-no_clusters = len(cluster_centers_indices)
+    # X , days_to_death = load_data()
+    # X = reduce_dim(X)
+    # Setup Affinity Propagation
+    af = AffinityPropagation(preference=-50).fit(X)
+    cluster_centers_indices = af.cluster_centers_indices_
+    labels = af.labels_
+    # print(labels)
+    # print(len(labels))
+    print(cluster_centers_indices)
+    no_clusters = len(cluster_centers_indices)
 
-print('Estimated number of clusters: %d' % no_clusters)
-# Plot exemplars
+    print('Estimated number of clusters: %d' % no_clusters)
+    # Plot exemplars
 
-plt.close('all')
-plt.figure(1)
-plt.clf()
+    # plt.close('all')
+    # plt.figure(1)
+    # plt.clf()
 
-colors = cycle('bgrcmykbgrcmykbgrcmykbgrcmyk')
-for k, col in zip(range(no_clusters), colors):
-    class_members = labels == k
-    cluster_center = X[cluster_centers_indices[k]]
-    plt.plot(X[class_members, 0], X[class_members, 1], col + '.')
-    plt.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col, markeredgecolor='k', markersize=14)
-    for x in X[class_members]:
-        plt.plot([cluster_center[0], x[0]], [cluster_center[1], x[1]], col)
+    colors = cycle('bgrcmykbgrcmykbgrcmykbgrcmyk')
+    for k, col in zip(range(no_clusters), colors):
+        class_members = labels == k
+        cluster_center = X[cluster_centers_indices[k]]
+        ax.plot(X[class_members, 0], X[class_members, 1], col + '.')
+        ax.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col, markeredgecolor='k', markersize=14)
+        for x in X[class_members]:
+            ax.plot([cluster_center[0], x[0]], [cluster_center[1], x[1]], col)
 
-plt.show()
+    return labels
--- kMeans.py
+++ kMeans.py
@@ -1,5 +1,7 @@
+import os
+from saveResults import save_fig
 # Import the kmeans clustering model.
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans,MiniBatchKMeans
 from readData import load_data
 # Import the PCA model.
 from sklearn.decomposition import PCA
@@ -7,35 +9,40 @@ import matplotlib.pyplot as plt
 import numpy as np
 from dim_reduction import reduce_dim
 
+def cluster(X,fig,ax):
 
-# Initialize the model with 2 parameters -- number of clusters and random state.
-kmeans_model = KMeans(n_clusters=8, random_state=1, n_init=10)
-# Get only the numeric columns from games.
-X, days_to_death = load_data()
-X = reduce_dim(X,2)
-print(type(days_to_death[0]))
-# Fit the model using the good columns.
-kmeans_model.fit(X)
-# Get the cluster assignments.
-labels = kmeans_model.labels_
-# print(labels)
+	num_clusters = int(input("Enter Number of cluster:"))
+	choice = int(input("kMeans/MiniBatchKMeans(0/1):"))
+	kmeans_model = ''
+	if choice == '0':
+		# Initialize the model with 2 parameters -- number of clusters and random state.
+		kmeans_model = KMeans(n_clusters=num_clusters, random_state=1, n_init=10)
+	else:
+		# Initialize the model with 2 parameters -- number of clusters and random state.
+		kmeans_model = MiniBatchKMeans(n_clusters=num_clusters, init='k-means++', n_init=1,
+						init_size=1000, batch_size=1000)
 
-clusters = {i: np.where(labels == i)[0] for i in range(kmeans_model.n_clusters)}
+	# Fit the model using the good columns.
+	kmeans_model.fit(X)
+	# Get the cluster assignments.
+	labels = kmeans_model.labels_
+	print(labels)
 
-print(clusters)
-
-choice = input("Want to apply PCA(0/1):")
-if choice == '0':
-	plt.scatter(x=X[:,0], y=X[:,1], c=labels)
-	plt.show()
-else:
-	# Create a PCA model.
-	pca_2 = PCA(2)
-	# Fit the PCA model on the numeric columns from earlier.
-	plot_columns = pca_2.fit_transform(X)
-	# Make a scatter plot of each game, shaded according to cluster assignment.
-	plt.scatter(x=plot_columns[:,0], y=plot_columns[:,1], c=labels)
-	# print(days_to_death.shape)
-	# plt.scatter(x=[i for i in range(len(clusters[0]))], y=days_to_death[clusters[0]])
-	# Show the plot.
-	plt.show()
+	clusters = {i: np.where(labels == i)[0] for i in range(kmeans_model.n_clusters)}
+	# print(clusters)
+	choice = input("Want to apply PCA(0/1):")
+	if choice == '0':
+		ax.scatter(x=X[:,0], y=X[:,1], c=labels)
+		# plt.show()
+	else:
+		# Create a PCA model.
+		pca_2 = PCA(2)
+		# Fit the PCA model on the numeric columns from earlier.
+		plot_columns = pca_2.fit_transform(X)
+		# Make a scatter plot of each game, shaded according to cluster assignment.
+		ax.scatter(x=plot_columns[:,0], y=plot_columns[:,1], c=labels)
+		# print(days_to_death.shape)
+		# plt.scatter(x=[i for i in range(len(clusters[0]))], y=days_to_death[clusters[0]])
+		# Show the plot.
+		# plt.show()
+	return labels
