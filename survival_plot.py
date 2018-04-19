215c227c53055ff53c23886b34e0a4ed54ed3268
Nilesh Vaishnav <nilesh.iitg@gmail.com>
2018-04-20 00:52:05 +0530

    Add cluster size in plot and color for survival functions

--- survival_plot.py
+++ survival_plot.py
@@ -7,39 +7,27 @@ from saveResults import COLORS
 
 def survival_func(clusters,days_to_death,death_observed,ax):
     model = KaplanMeierFitter()
-    # print(len(days_to_death),len(death_observed))
-    # print(clusters[0])
-    # print(days_to_death[clusters[0]])
-    # print(death_observed[clusters[0]])
     num_clusters = len(clusters.keys())
     for i in range(num_clusters):
-        if len(clusters[i]) :
+        print(type(clusters[i]))
+        if len(clusters[i]) > 1 :
             print(clusters[i])
-            model.fit(days_to_death[clusters[i]], event_observed=death_observed[clusters[i]], label='KM_estimate for cluster:'+str(i))
-            # if i == 0:
-                # ax = model.survival_function_.plot()
-            # else:
-            model.survival_function_.plot(ax=ax,color=COLORS[i])
+            model.fit(days_to_death[clusters[i]], event_observed=death_observed[clusters[i]], label='cluster:'+str(i+1) + '(' + str(len(clusters[i])) + ')')
+            model.survival_function_.plot(ax=ax,color=COLORS[i%len(COLORS)])
             print('Done for cluster ',i)
     ax.set_title('Survival function of cluster');
 
 def hazard_func(clusters,days_to_death,death_observed,ax):
     model = AalenAdditiveFitter(coef_penalizer=1.0, fit_intercept=True)
-    # print(len(days_to_death),len(death_observed))
-    # print(clusters[0])
-    # print(days_to_death[clusters[0]])
-    # print(death_observed[clusters[0]])
     num_clusters = len(clusters.keys())
     for i in range(num_clusters):
-        if len(clusters[i]) :
+        # print(clusters[i])
+        if len(clusters[i]) > 1 :
             X = {}
             X['days_to_death'] = days_to_death[clusters[i]]
             X['death_observed'] = death_observed[clusters[i]]
             X = pd.DataFrame(X)
             model.fit(X, 'days_to_death', event_col='death_observed')
-            # if i == 0:
-                # ax = model.plot()
-            # else:
-            model.plot(ax=ax,color=COLORS[i])
+            model.plot(ax=ax,color=COLORS[i%len(COLORS)])
             print('Done for cluster ',i)
     ax.set_title('Cumulatice hazard of cluster');
