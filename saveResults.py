a9f808bd5b52b8a59f4e343c80a9ebb08dd40616
Nilesh Vaishnav <nilesh.iitg@gmail.com>
2018-04-20 00:50:39 +0530

    Add plotting function

--- saveResults.py
+++ saveResults.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plot
 import matplotlib._color_data as mcd
 import matplotlib.patches as mpatch
 
-COLORS = ['crimson', 'indigo', 'pink', 'chartreuse', 'olive', 'aqua',
+COLORS = ['crimson', 'indigo', 'chartreuse', 'olive', 'aqua',
         'orangered', 'blue', 'tan', 'silver', 'gold', 'goldenrod',
         'violet', 'maroon', 'brown', 'magenta', 'fuchsia', 'black',
         'coral', 'salmon', 'yellow', 'cyan', 'darkblue', 'orchid',
@@ -23,3 +23,12 @@ def save_fig(algo,fig):
     DefaultSize = fig.get_size_inches()
     fig.set_size_inches( (DefaultSize[0]*2, DefaultSize[1]*2) )
     fig.savefig(foldername + t + '.png',bbox_inches='tight')
+
+def plot1(num_clusters,labels,X,centers,ax):
+    for k, col in zip(range(num_clusters), COLORS):
+        class_members = labels == k
+        cluster_center = centers[k]
+        ax.plot(X[class_members, 0], X[class_members, 1], '.', color=col)
+        ax.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col, markeredgecolor='k', markersize=14)
+        for x in X[class_members]:
+            ax.plot([cluster_center[0], x[0]], [cluster_center[1], x[1]], col)
